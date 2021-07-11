9052faf66ecb5973f9d6bcc850f0e30ed37a77c5
Assaf Sapir <assapir@github.com>
2021-07-11 23:03:19 +0300

    checkout different

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const { Pool } = require("pg")
+
+const pool = new Pool({ connectionString })
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
