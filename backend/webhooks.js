471da2d43ea41ef28d22b63d0314717a09bceade
Assaf Sapir <me@ass.af>
2021-09-30 17:28:40 +0300

    Rollout on deployment

--- backend/webhooks.js
+++ backend/webhooks.js
@@ -28,7 +28,7 @@ router.post('/', async (req, res) => {
 
     const platforms = JSON.parse(labels?.all_labels['github.internal.platforms'])
     if (!platforms?.some(p => p.architecture === 'amd64')) {
-      console.warn('Not amd64, ignoring')
+      console.warn('Not amd64, ignoring :(')
       return res.sendStatus(204)
     }
 
